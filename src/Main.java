1aeb081f23a53eacdb21c9ed5de25f795efe7555
Mustafa <Ycesan>
2023-11-23 11:06:48 +0100

    Correct algorithm now. Had to sort on starting time rather than earliest deadline because of having multiple resources, you want to repair the bikes as they come in.
    This is a quadratic solution, there is a linear solution possible by sorting the start and end times separately.

--- src/Main.java
+++ src/Main.java
@@ -6,21 +6,24 @@ public class Main {
 //    }
 
     public static int fixMyBikesPlease(int n, int[] starttimes, int[] durations) {
-        if (n == 0) return 0;
-        int employeesNeeded = 1;
-        Bike[] bikes = new Bike[n + 1]; // n + 1 because zero-indexed
+        int employeesNeeded = 0;
+        Bike[] bikes = new Bike[n + 1];
         for (int i = 1; i <= n; i++) {
             bikes[i] = new Bike(starttimes[i], durations[i]);
         }
-        Arrays.sort(bikes, 1, n + 1); // earliest finishing time first
-        int currentEndtime = bikes[1].getFinishingTime();
+        Arrays.sort(bikes, 1 , n + 1); // earliest finishing time first
         for (int i = 2; i <= n; i++) {
-            if (currentEndtime > bikes[i].getStartTime()) {
-                employeesNeeded++;
-            } else {
-                currentEndtime = bikes[i].getFinishingTime();
+            int count =  1;
+            for (int j = 1; j < i; j++) {
+                if (bikes[j].getFinishingTime() > bikes[i].getStartTime()) {
+                    count++;
+                }
+            }
+            if (count > employeesNeeded){
+                employeesNeeded = count;
             }
         }
+
 
         return employeesNeeded;
     }
@@ -48,6 +51,6 @@ class Bike implements Comparable<Bike> {
 
     @Override
     public int compareTo(Bike b) {
-        return Integer.compare(this.getFinishingTime(), b.getFinishingTime());
+        return Integer.compare(this.getStartTime(), b.getStartTime());
     }
 }
